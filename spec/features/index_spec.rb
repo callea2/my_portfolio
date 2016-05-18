c476b73a2a4df5e6d259e007550e983267ed7e05
Carl-Philip <callea2@gmail.com>
2016-05-18 11:49:15 +0200

    Added test for header and made it pass

--- spec/features/index_spec.rb
+++ spec/features/index_spec.rb
@@ -16,4 +16,12 @@ describe 'index', type: :feature do
   it 'displays Calle image' do
     expect(page).to have_css 'img[src*="calle.jpg"]'
   end
+
+  it 'renders header partial' do
+    expect(page).to have_selector 'header'
+    within 'header' do
+      expect(page).to have_content 'Home'
+      expect(page).to have_css 'img[src*="logo.png"]'
+    end
+  end
 end
