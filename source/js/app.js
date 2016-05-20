db63c284c3d5a83e5bf2e0d18261238659c462c3
Carl-Philip <callea2@gmail.com>
2016-05-20 11:21:54 +0200

    Updated hero image, WIP: Stick footer to bottom of page

---
+++ source/js/app.js
@@ -0,0 +1,23 @@
+function fixFooter() {
+    var footer = $("#footer");
+    height = footer.height();
+    paddingTop = parseInt(footer.css('padding-top'), 10);//zero if is undefined
+    paddingBottom = parseInt(footer.css('padding-bottom'), 10);//zero if is undefined
+    totalHeight = (height + paddingTop + paddingBottom);
+    footerPosition = footer.position();
+    windowHeight = $(window).height();
+    height = (windowHeight - footerPosition.top) - totalHeight;
+    if (height > 0) {
+        footer.css({
+            'margin-top': (height) + 'px'
+        });
+    }
+}
+
+$(document).ready(function () {
+    fixFooter();
+});
+
+$(window).resize(function () {
+    fixFooter();
+});
